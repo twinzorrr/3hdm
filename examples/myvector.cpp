41b8fa85641432134e8452d4acf2f1643b82bcc8
twinzorrr <73521766+twinzorrr@users.noreply.github.com>
2020-11-05 14:41:07 +0100

    remove using namespace.

--- examples/myvector.cpp
+++ examples/myvector.cpp
@@ -10,44 +10,41 @@
 #include "group.h"
 #include "yukawa.h"
 
-using namespace std;
-using namespace Eigen;
-
 
 int main() {
 
-	using cd = complex<double>;
+	using cd = std::complex<double>;
 
-	VectorXcd v(27);
+	Eigen::VectorXcd v(27);
 	v << cd(0, 0), cd(0, 0), cd(0, 0), cd(0, -5.55112e-17), cd(0, 0), cd(0.0911973, -0.157958), cd(0, 0), cd(0, 0), cd(0, 0),
 		cd(0, 0), cd(0, 0), cd(0, 0), cd(0, 0), cd(0, 0), cd(0, 0), cd(-0.182395, -5.71479e-09), cd(-5.55112e-17, -1.11022e-16), cd(0, 0),
 		cd(0, 0), cd(0.0911973, 0.157958), cd(-4.16334e-17, 2.22045e-16), cd(0, 0), cd(0, 0), cd(0, 0), cd(0, 0), cd(0, 0), cd(0, 0);
 
 	MyVector mv(v);
-	vector<double> vd;
+	std::vector<double> vd;
 
-	cout << "Input myvector: " << endl << mv << endl << endl;
+	std::cout << "Input myvector: " << std::endl << mv << std::endl << std::endl;
 
-	cout << "getNumberofElements: " << mv.getNumberofElements() << endl << endl;
+	std::cout << "getNumberofElements: " << mv.getNumberofElements() << std::endl << std::endl;
 
 	mv.setNumericZerotoActualZero();
-	cout << "setNumericZerotoActualZero: " << endl << mv << endl << endl;
+	std::cout << "setNumericZerotoActualZero: " << std::endl << mv << std::endl << std::endl;
 
 	mv.setFirstNonZeroElementto1();
-	cout << "setFirstNonZeroElementto1: " << endl << mv << endl << endl;
+	std::cout << "setFirstNonZeroElementto1: " << std::endl << mv << std::endl << std::endl;
 
 	mv = mv.setAllNonZeroElementstoPhases();
-	cout << "setAllNonZeroElementstoPhases: " << endl << mv << endl << endl;
+	std::cout << "setAllNonZeroElementstoPhases: " << std::endl << mv << std::endl << std::endl;
 
-	cout << "getPhases: ";  for (auto& i : mv.getPhases()) cout << i << " "; cout << endl << endl;
+	std::cout << "getPhases: ";  for (auto& i : mv.getPhases()) std::cout << i << " "; std::cout << std::endl << std::endl;
 
-	cout << "getMassMatrix (with default parameters): " << endl << mv.getMassMatrix() << endl << endl;
+	std::cout << "getMassMatrix (with default parameters): " << std::endl << mv.getMassMatrix() << std::endl << std::endl;
 
-	cout << "getMassRatio (with step equal to 10.0): " << endl;
-	mv.getMassRatio(10.0, vd); for (auto i : vd) cout << i << " "; cout << endl << endl;
+	std::cout << "getMassRatio (with step equal to 10.0): " << std::endl;
+	mv.getMassRatio(10, vd); for (auto i : vd) std::cout << i << " "; std::cout << std::endl << std::endl;
 
 	mv = mv.setAllNonZeroElementsto1();
-	cout << "setAllNonZeroElementsto1: " << endl << mv << endl;
+	std::cout << "setAllNonZeroElementsto1: " << std::endl << mv << std::endl;
 
 	return EXIT_SUCCESS;
 
